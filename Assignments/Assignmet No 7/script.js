b5671ce85d3b1fb180601b2f86da648a1e4ed800
Sabar Ali <maliksabarali3@gmail.com>
2024-04-03 17:12:04 +0500

    assignment

---
+++ Assignments/Assignmet No 7/script.js
@@ -0,0 +1,47 @@
+// // 1. Write a js program to find the maximum between two numbers.
+
+// const number1 = prompt("Enter first number");
+// const number2 = prompt("Enter second number");
+
+// if (number2 > number1) {
+//     console.log('${number2} is greater then ${number1}')
+// } else if (number1 > number2) {
+//     console.log('${number1} is greater then ${number2}')
+// } else {
+//     alert('${number1} is equal to ${number2}')
+// }
+
+
+
+// // 2. Write a js program to find the maximum between three numbers.
+
+// const num1 = prompt("Enter first num");
+// const num2 = prompt("Enter second num");
+// const num3 = prompt("Enter third num"); 
+
+// if (num1 > num2 && num1 > num3){
+//     console.log('${num1} is greater then ${num2} and ${num3}')
+// }else if(num2 > num1 && num2 > num3){
+//     console.log('${num2} is greater then ${num1} and ${num3}')
+// }else if(num3 > num1 && num3 > num2){
+//     console.log('${num3} is greater then ${num1} and ${num2}')
+// }else{
+//     console.log('All the nums are equal')
+// }
+
+
+
+// // 3. Write a js program to check whether a number is negative, positive or zero.
+
+// const price1 = prompt("Enter the price");
+
+// if(price1 > 0){
+//     console.log('Positive')
+// }else if(price1 < 0){
+//     console.log('Negative')
+// }else{
+//     console.log('0')
+// }
+
+
+// 4. Write a js program to check whether a number is divisible by 5 and 11 or not.
